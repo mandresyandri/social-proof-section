b0897a8de8d83b97e5f1c40f48658e21eddfbf6d
mandresyandri <manjess.andri@icloud.com>
2022-11-12 15:29:43 +0100

    Init project with react

---
+++ src/Attribution.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Attribution = () => {
+    return (
+        <div className="attribution">
+            Challenge by
+            <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+            rel="noreferrer">
+            Frontend Mentor
+            </a>. Coded by
+            <a href="https://github.com/mandresyandri" target="_blank" rel="noreferrer">
+            Mandresy ANDRI
+            </a>.
+        </div>
+    );
+};
+
+export default Attribution;
---
+++ src/Content.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./index.css"
+
+const Content = () => {
+    return (
+        <div>
+            <h2 className="headTitle">10,000+ of our users love our products.</h2>
+
+            <p>We only provide great products combined with excellent customer service.
+            See what our satisfied customers are saying about our services.</p>
+        </div>
+    );
+};
+
+export default Content;
