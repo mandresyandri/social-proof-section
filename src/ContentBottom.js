fc47119618e9a53580517ddbb035eb279db6f45c
mandresyandri <manjess.andri@icloud.com>
2022-11-13 23:02:21 +0100

    Update : Ok for Desktop it's time for mobile part

--- src/ContentBottom.js
+++ src/ContentBottom.js
@@ -11,6 +11,13 @@ const ContentBottom = () => {
                         <div className="colored">Verified Buyer</div>
                     </div>
                 </div>
+                <div className="bottom-content">
+                    <p>
+                        "We needed the same printed design as the one we had ordered a week prior.
+                        Not only did they find the original order, but we also received it in time.
+                        Excellent!"
+                    </p>
+                </div>
             </div>
             <div className="second boxTwo">
                 <div className="top-content flex">
@@ -20,6 +27,12 @@ const ContentBottom = () => {
                         <div className="colored">Verified Buyer</div>
                     </div>
                 </div>
+                <div className="bottom-content">
+                    <p>
+                        "Customer service is always excellent and very quick turn around. Completely
+                        delighted with the simplicity of the purchase and the speed of delivery."
+                    </p>
+                </div>
             </div>
             <div className="third boxTwo">
                 <div className="top-content flex">
@@ -28,6 +41,12 @@ const ContentBottom = () => {
                         <div className="name-text">Anne Wallace</div>
                         <div className="colored">Verified Buyer</div>
                     </div>
+                </div>
+                <div className="bottom-content">
+                    <p>
+                        "Put an order with this company and can only praise them for the very high
+                        standard. Will definitely use them again and recommend them to everyone!"
+                    </p>
                 </div>
             </div>
         </div>
